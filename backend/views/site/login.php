f1e85289797b093817fa57dda4d8c2783b9035e4
Ahmad Khan <ahmad.khan@cusolutionsgroup.com>
2017-10-25 19:05:49 -0400

    login page
    login page

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -1,31 +1,33 @@
-<?php
 
+<?php
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
-
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-
-$this->title = 'Menu';
+$this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-menu">
+<div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
-    <div class="menu" >
 
-            <!--Selection BAR -->
-            <div class="col-md-3">
-                <p class="lead">Create Page</p>
-                <div class="list-group">
-                    <a class="list-group-item">Menus</a>
-                    <a class="list-group-item">Pages</a>
-                    <a class="list-group-item">Pictures</a>
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
-           </div>
-         </div>
 
-
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
 </div>
