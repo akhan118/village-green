83391b9ec3bded86dbef3b68e7aa1e743e13eb60
Nidra <deesmity7@yahoo.com>
2017-12-10 21:03:29 -0500

    update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -106,7 +106,7 @@ AppAsset::register($this);
                                     <div class="main-menu">
                                         <ul class="nav navbar-nav navbar-right">
                                             <li><a href=<?php echo Url::to(['site/index']) ?>><?
-if(isset($this->params['menu'])){echo $this->params['menu'][0]['index'];}
+if(isset($this->params['menu'])){echo $this->params['menu'][0]['department'];}
 // var_dump($this->params['menu'][0]['menu_name']);
 
                                             //echo $this->params['menu'];?></a>
@@ -116,7 +116,7 @@ if(isset($this->params['menu'])){echo $this->params['menu'][0]['index'];}
                                             <li><a href=<?php echo Url::to(['site/about']) ?>>About</a></li>
                                             <li><a class="pagess" href=<?php echo Url::to(['site/hardware']) ?>>Hardware</a>
 
-                                            <li><a href="about.html"><? if(isset($this->params['menu'])){echo $this->params['menu'][1]['hardware'];}?></a></li>
+                                            <li><a href="about.html"><? if(isset($this->params['menu'])){echo $this->params['menu'][1]['department'];}?></a></li>
                                             <li><a class="pagess" href=<?php echo Url::to(['site/department']) ?>>Hardware</a>
 
                                                 <ul class="sub-menu sub-1">
