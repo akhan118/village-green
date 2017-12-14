50065af274666ddb19929849bfc26f8330ad59d7
Ismael Xique <ixique@umich.edu>
2017-12-13 20:59:41 -0500

    update the pages editor
    add plugins for ckeditor

--- backend/views/app/pages.php
+++ backend/views/app/pages.php
@@ -7,30 +7,51 @@ use yii\bootstrap\Modal;
 
 $this->title = 'Pages';
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerJs("CKEDITOR.plugins.addExternal('colorbutton', '/village-green/backend/web/ckeditor/plugins/colorbutton/plugin.js', '');");
+$this->registerJs("CKEDITOR.plugins.addExternal('font', '/village-green/backend/web/ckeditor/plugins/font/plugin.js', '');");
+$this->registerJs("CKEDITOR.plugins.addExternal('image2', '/village-green/backend/web/ckeditor/plugins/image2/plugin.js', '');");
+$this->registerJs("CKEDITOR.plugins.addExternal('widget', '/village-green/backend/web/ckeditor/plugins/widget/plugin.js', '');");
+$this->registerJs("CKEDITOR.plugins.addExternal('imageuploader', '/village-green/backend/web/ckeditor/plugins/imageuploader/plugin.js', '');");
+
 ?>
 
 
-<?php $form = ActiveForm::begin(); ?>
+  <?php $form = ActiveForm::begin(); ?>
 
-<div class="container"
-    <div class="row" >
-        <div class="col-md-7">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-1">
+      </div>
+      <div class="col-md-10">
 
-        <?= $form->field($model,'page_id') -> textInput(['maxlength'=>"True"]) ?>
-        <?= $form->field($model, 'page_html')->widget(CKEditor::className(), [
-                       'options' => ['rows' => 2],
-                       'preset' => 'advance'
+        <?= $form->field($model, 'page_id') -> textInput(['maxlength'=>"True"]) ?>
+        <br>
+
+          <?= $form->field($model, 'page_html')->widget(CKEditor::className(), [
+                       'clientOptions' => [
+                      'extraPlugins' => 'colorbutton,font,image2,widget,imageuploader',
+                        'toolbarGroups' => [
+                            ['name' => 'undo'],
+                            ['name' => 'colors'],
+                            ['name' => 'links', 'groups' => ['links', 'insert']],
+                            ['name' => 'others', 'groups' => ['others', ]],
+                            ['name' => 'document', 'groups' => ['mode', ]],
+                            ['name' => 'paragraph', 'groups' => ['list','blocks']], // <--- OUR NEW PLUGIN YAY!
+                            ['name' => 'styles'], // <--- OUR NEW PLUGIN YAY!
+
+                        ]
+                    ],
+                       'preset' => 'custom'
                    ]) ?>
-        <br>
-        <br>
+              <br>
+              <br>
 
-        <div class="form-group">
-          <?= Html::submitButton('Create', ['class' => 'btn btn-primary', 'name' => 'create-button']) ?>
-        </div>
-
-        </div>
-        </div>
-
-          <?php ActiveForm::end() ?>
-          <code><?= __FILE__ ?></code>
-</div>
+              <div class="form-group">
+                <?= Html::submitButton('Create', ['class' => 'btn btn-primary', 'name' => 'create-button']) ?>
+              </div>
+      </div>
+      <div class="col-md-1">
+      </div>
+    </div>
+    <?php ActiveForm::end() ?>
+  </div>
