8aff2e32c8f8643f353198090e47dc3f952ab6ec
Ismael Xique <ixique@umich.edu>
2017-12-30 14:58:19 -0500

    update front end department view
    update front end department view

--- frontend/views/site/department.php
+++ frontend/views/site/department.php
@@ -9,7 +9,6 @@ use yii\helpers\Html;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-    <?  var_dump($results)  ?>
 
     <!-- service area start -->
     	<!-- Start Slider Area -->
@@ -32,16 +31,7 @@ use yii\helpers\Html;
                 <div class="row">
                     <div class="col-md-3 col-sm-5 col-xs-12">
                         <div class="page-head-left">
-                            <div class="single-page-head">
-                                <div class="left-menu">
-                                    <ul>
-                                        <li class="active"><a href="sub-services-1.html">Hardware</a></li>
-                                        <li><a href="sub-services-2.html">Lawn &amp; Garden</a></li>
-                                        <li><a href="sub-services-3.html">House &amp; Gifts</a></li>
-                                        <li><a href="sub-services-4.html">Specialty Items</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+
                             <div class="single-page-head">
                                 <div class="left-contact">
                                     <h4 class="sec-head">Contact Information</h4>
@@ -62,91 +52,64 @@ use yii\helpers\Html;
                                     </div>
                                 </div>
                             </div>
-                            <!-- <div class="single-page-head">
-                                <div class="left-contact">
-                                    <h4 class="sec-head">Send Message</h4>
-                                    <div class="contact-form text-center">
-                                        <form id="contactForm" method="POST" action="contact.php" class="contact-form">
-                                            <input type="text" id="name" class="form-control" placeholder="Name" required data-error="Please enter your name">
-                                            <div class="help-block with-errors"></div>
-                                            <input type="email" class="email form-control" id="email" placeholder="Email" required data-error="Please enter your email">
-                                            <div class="help-block with-errors"></div>
-                                            <textarea id="message" rows="7" placeholder="Massage" class="form-control" required data-error="Write your message"></textarea>
-                                            <div class="help-block with-errors"></div>
-                                            <button type="submit" id="submit" class="add-btn contact-btn">Submit</button>
-                                            <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                            <div class="clearfix"></div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div> -->
+
                         </div>
                     </div>
                     <!-- End left sidebar -->
                     <!-- Start service page -->
                     <div class="col-md-9 col-sm-7 col-xs-12">
                         <div class="row">
-                            <!-- <div class="col-md-12 col-sm-12 col-xs-12">
-                                <div class="single-page">
-                                    <div class="page-img elec-page">
-                                        <img src="img/services/ser1.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div> -->
-                            <!-- strat single page -->
-                            <!-- single-well end-->
-                            <div class="col-md-6 col-sm-12 col-xs-12">
-                                <div class="single-well">
-                                    <a href="#">
-                                        <img src="img/services/ser2.jpg" alt="">
-                                        <h4 class="sec-head">Perfect Planning</h4>
-                                    </a>
-                                    <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-                                    <a class="page-btn" href="#"> Read more</a>
-                                </div>
-                            </div>
-                            <!-- End single page -->
-                            <!-- strat single page -->
-                            <div class="col-md-6 col-sm-12 col-xs-12">
-                                <div class="single-well">
-                                    <a href="#">
-                                        <img src="img/services/ser4.jpg" alt="">
-                                        <h4 class="sec-head">Perfect Finshing</h4>
-                                    </a>
-                                    <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-                                    <a class="page-btn" href="#"> Read more</a>
-                                </div>
-                            </div>
-														<div class="col-md-6 col-sm-12 col-xs-12">
-																<div class="single-well">
-																		<a href="#">
-																				<img src="img/services/ser4.jpg" alt="">
-																				<h4 class="sec-head">Perfect Finshing</h4>
-																		</a>
-																		<p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-																		<a class="page-btn" href="#"> Read more</a>
-																</div>
-														</div>
-														<div class="col-md-6 col-sm-12 col-xs-12">
-																<div class="single-well">
-																		<a href="#">
-																				<img src="img/services/ser4.jpg" alt="">
-																				<h4 class="sec-head">Perfect Finshing</h4>
-																		</a>
-																		<p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-																		<a class="page-btn" href="#"> Read more</a>
-																</div>
-														</div>
-                            <!-- End single page -->
-                            <!-- <div class="clear"></div>
-                            <div class="col-md-12 col-sm-12 col-xs-12">
-                                <div class="single-well">
-                                    <h4 class="sec-head">Perfect Planning For Internation Business Law</h4>
-                                    <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-                                    <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
-                                </div>
-                            </div>
-                        </div> -->
+
+                          <? if ( isset($results[0]) ){
+                           echo ($results[0]["page_html"]); }
+                           else { ?>
+
+
+                             <div class="col-md-6 col-sm-12 col-xs-12">
+                                 <div class="single-well">
+                                     <a href="#">
+                                         <img src="img/services/ser2.jpg" alt="">
+                                         <h4 class="sec-head">Perfect Planning</h4>
+                                     </a>
+                                     <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
+                                     <a class="page-btn" href="#"> Read more</a>
+                                 </div>
+                             </div>
+                             <!-- End single page -->
+                             <!-- strat single page -->
+                             <div class="col-md-6 col-sm-12 col-xs-12">
+                                 <div class="single-well">
+                                     <a href="#">
+                                         <img src="img/services/ser4.jpg" alt="">
+                                         <h4 class="sec-head">Perfect Finshing</h4>
+                                     </a>
+                                     <p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
+                                     <a class="page-btn" href="#"> Read more</a>
+                                 </div>
+                             </div>
+ 														<div class="col-md-6 col-sm-12 col-xs-12">
+ 																<div class="single-well">
+ 																		<a href="#">
+ 																				<img src="img/services/ser4.jpg" alt="">
+ 																				<h4 class="sec-head">Perfect Finshing</h4>
+ 																		</a>
+ 																		<p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
+ 																		<a class="page-btn" href="#"> Read more</a>
+ 																</div>
+ 														</div>
+ 														<div class="col-md-6 col-sm-12 col-xs-12">
+ 																<div class="single-well">
+ 																		<a href="#">
+ 																				<img src="img/services/ser4.jpg" alt="">
+ 																				<h4 class="sec-head">Perfect Finshing</h4>
+ 																		</a>
+ 																		<p>When replacing a multi-lined selection of text, the generated dummy text maintains the amount of lines. When replacing a selection of text within a single line, the amount of words is roughly being maintained.When the replaced text selection consists fully of lower-cased or capital letters or begins with a capital letter, that previous casing is maintained. Furthermore, the presence or absence of a trailing punctuation mark of a replaced text selection is being maintained.</p>
+ 																		<a class="page-btn" href="#"> Read more</a>
+ 																</div>
+ 														</div>
+
+                           <? }  ?>
+
                     </div>
                 </div>
             </div>
