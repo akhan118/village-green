1a827135e316a2142459379668568291905f14c2
YSC <technosayge@gmail.com>
2018-02-19 21:35:30 -0500

    updating code to give Coding Crash Course credit.

--- frontend/views/site/houseandgifts.php
+++ frontend/views/site/houseandgifts.php
@@ -168,7 +168,7 @@ use yii\helpers\Html;
                       </div>
 			            <div class="footer-bottom">
                             <div class="copyright text-center">
-                                <p>Copyright © 2016 <a href="#"> usdtheme</a>. All Rights Reserved</p>
+                                <p>Copyright © 2018 <a href="https://www.meetup.com/Coding-Crash-Course/"> Coding Crash Course</a>. All Rights Reserved</p>
                             </div>
                         </div>
 				   </div>
